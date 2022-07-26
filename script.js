13527ee0a7a67a0e9ea1ad78292aea6949d026cf
Somanimus <kavosm325@gmail.com>
2022-07-26 14:54:02 +0600

    added sorting mechanics, added pagination, fixed per page option

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
+// this is a for a container of the main content
 const contentDisplay = document.querySelector('.results-container');
+// this is for a main container
+
+// the next section exists to read enter input from a bunch of input tags
 document.querySelector('#result-button')
     .addEventListener('click', function() {
       addContent();
     });
 document.querySelector('.search-input').onkeydown = function(e) {
   if (e.keyCode == 13) {
+    document.querySelector('.pagination-input').value = 1;
     addContent();
   }
 };
+document.querySelector('.previous-button').onclick = function(e) {
+  document.querySelector('.pagination-input').value -= 1;
+  addContent();
+};
+document.querySelector('.page-num-input').onkeydown = function(e) {
+  if (e.keyCode == 13) {
+    addContent();
+  }
+};
+document.querySelector('.next-button').onclick = function(e) {
+  const num = parseInt(1);
+  const num2 = parseInt(document.querySelector('.pagination-input').value);
+  document.querySelector('.pagination-input').value = num2 + num;
+  addContent();
+};
+document.querySelector('.pagination-input').onkeydown = function(e) {
+  if (e.keyCode == 13) {
+    addContent();
+  }
+};
+document.querySelector('.sorter-select').onchange = function() {
+  addContent();
+};
+document.querySelector('.order-select').onchange = function() {
+  addContent();
+};
 
+
+// this section renders results of search
 addContent =async () => {
   contentDisplay.innerHTML = ``;
-  const dataArr =await gitFetchRequest().then(data=>data.items);
+  const dataArr =await gitFetchRequest().then((data)=>data.items);
   console.log(dataArr);
   for (let i = 0; i < dataArr.length; i++) {
     contentDisplay.innerHTML += `
@@ -29,14 +62,20 @@ addContent =async () => {
   };
 };
 
+// this section is for sending a fetch request to Github API
 gitFetchRequest = async () => {
   const query = document.querySelector('.search-input').value;
   const itemAmount = document.querySelector('.page-num-input').value;
-  const URL = `https://api.github.com/search/users?q=${query}&per_page=${itemAmount}&page2}`;
+  const queryPage = document.querySelector('.pagination-input').value;
+  const sorterSelected = document.querySelector('#sorter-select').value;
+  const orderSelected = document.querySelector('#order-select').value;
+
+  const URL = `https://api.github.com/search/users?q=${query}&sort=${sorterSelected}&order=${orderSelected}&per_page=${itemAmount}&page=${queryPage}`;
   const response = await fetch(URL).then((response) =>
     response.json()).then((data) => data);
-  console.log(query);
-  console.log(itemAmount);
+  console.log(response);
+  console.log(queryPage);
+  console.log(sorterSelected);
   return response;
 };
 
