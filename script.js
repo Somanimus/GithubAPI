5eb571fd0cb611a529ebf929f797c4c87d36cd07
Somanimus <kavosm325@gmail.com>
2022-07-27 14:38:58 +0600

    done repos popup, started favorites tab

--- script.js
+++ script.js
@@ -3,10 +3,7 @@ const contentDisplay = document.querySelector('.results-container');
 // this is for a main container
 
 // the next section exists to read enter input from a bunch of input tags
-document.querySelector('#result-button')
-    .addEventListener('click', function() {
-      addContent();
-    });
+
 document.querySelector('.search-input').onkeydown = function(e) {
   if (e.keyCode == 13) {
     document.querySelector('.pagination-input').value = 1;
@@ -14,8 +11,10 @@ document.querySelector('.search-input').onkeydown = function(e) {
   }
 };
 document.querySelector('.previous-button').onclick = function(e) {
-  document.querySelector('.pagination-input').value -= 1;
+  const val = document.querySelector('.pagination-input').value;
   addContent();
+  if (val < 1) val = 1; // minimum is 1
+  else val = Math.floor(val); // only integers allowed
 };
 document.querySelector('.page-num-input').onkeydown = function(e) {
   if (e.keyCode == 13) {
@@ -40,6 +39,26 @@ document.querySelector('.order-select').onchange = function() {
   addContent();
 };
 
+// this section is for changing tabs
+const searchButton = document.querySelector('.search-button');
+const favoritesButton = document.querySelector('.favorites-button');
+const searchSection = document.querySelector('.search-tab-container');
+const favoritesSection = document.querySelector('.favorites-tab-container');
+searchButton.addEventListener('click', function a() {
+  searchButton.classList.add('btn-selected');
+  favoritesButton.classList.remove('btn-selected');
+
+  favoritesSection.classList.add('hide-section');
+  searchSection.classList.remove('hide-section');
+});
+favoritesButton.addEventListener('click', function a() {
+  searchButton.classList.remove('btn-selected');
+  favoritesButton.classList.add('btn-selected');
+
+  favoritesSection.classList.remove('hide-section');
+  searchSection.classList.add('hide-section');
+});
+
 
 // this section renders results of search
 addContent =async () => {
@@ -55,10 +74,59 @@ addContent =async () => {
                 ${dataArr[i].login} 
               </h3>
             <a href="${dataArr[i].html_url}"
-             class="account-link">see account</a>
+             class="account-link" target="_blank">see account</a>
+             <button class="repos-button" onClick="showRepos(this.value,
+               this.id)"
+             id="${dataArr[i].login}"
+             value="${dataArr[i].repos_url}"
+             >
+             See Repositories</button>
+             </div>
+             <div class="popup" id="pop${dataArr[i].id}">
+
              </div>
        </div>
         `;
+    showRepos = async (repos, login) => {
+      const popUp = document.querySelector(`#pop${dataArr[i].id}`);
+      const URL = `${repos}`;
+      const response = await fetch(URL).then((response) =>
+        response.json()).then((data) => data);
+      console.log(response);
+
+      popUp.innerHTML = ``;
+      console.log(popUp);
+      popUp.classList.add('show');
+      popUp.innerHTML += `<h3 class="popup-headline">
+             repositories of ${login}</h3>
+             <img class="pop-close-btn" id="id${dataArr[i].id}"
+             src="./media/close.png" alt=""
+             style="width:20px;height:20px">
+             `;
+      for (let i = 0; i < response.length; i++) {
+        popUp.innerHTML+= `
+          <div class="repos-item">
+          <div class="repos-headline">
+          <h4>${response[i].name}</h4>
+          <p class="repos-date"> created ${response[i].created_at}</p>
+          </div>
+          <div class="repos-text-box">
+          <p> main language: 
+          ${response[i].language ? response[i].language: ''}<p>
+          <a href="${response[i].html_url}" target="_blank">see repository</a>
+          </div>
+          </div>
+       `;
+      }
+      if (response.length == 30) {
+        popUp.innerHTML+= `<a href="https://github.com/${login}?tab=repositories">see all</a>`;
+      }
+      console.log(popUp.classList);
+      const closeBtn = document.querySelector(`#id${dataArr[i].id}`);
+      closeBtn.addEventListener('click', function() {
+        popUp.classList.remove('show');
+      });
+    };
   };
 };
 
@@ -73,9 +141,7 @@ gitFetchRequest = async () => {
   const URL = `https://api.github.com/search/users?q=${query}&sort=${sorterSelected}&order=${orderSelected}&per_page=${itemAmount}&page=${queryPage}`;
   const response = await fetch(URL).then((response) =>
     response.json()).then((data) => data);
-  console.log(response);
-  console.log(queryPage);
-  console.log(sorterSelected);
+
   return response;
 };
 
